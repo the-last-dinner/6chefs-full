758219d9dbadab6ec760243f6e58fc5d7dc906a9
RyoyaIno <inos.sj@gmail.com>
2015-06-28 15:05:13 +0900

    playerDataManagerの作成

---
+++ Classes/Managers/PlayerDataManager.cpp
@@ -0,0 +1,9 @@
+//
+//  PlayerDataManager.cpp
+//  LastSupper
+//
+//  Created by 猪野凌也 on 2015/06/28.
+//
+//
+
+#include "PlayerDataManager.h"
---
+++ Classes/Managers/PlayerDataManager.h
@@ -0,0 +1,14 @@
+//
+//  PlayerDataManager.h
+//  LastSupper
+//
+//  Created by 猪野凌也 on 2015/06/28.
+//
+//
+
+#ifndef __LastSupper__PlayerDataManager__
+#define __LastSupper__PlayerDataManager__
+
+#include <stdio.h>
+
+#endif /* defined(__LastSupper__PlayerDataManager__) */
