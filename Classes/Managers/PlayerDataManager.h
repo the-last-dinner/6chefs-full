c7c951bb40da8728ed7ab2e76aedcb7e3b774e5b
RyoyaIno <inos.sj@gmail.com>
2015-07-20 17:00:02 +0900

    global, localデータのセーブとロード機能の追加

--- Classes/Managers/PlayerDataManager.h
+++ Classes/Managers/PlayerDataManager.h
@@ -13,6 +13,56 @@
 
 class PlayerDataManager
 {
+//構造体
+public:
+    struct SaveIndex
+    {
+        int data_id;
+        string name;
+        string play_time;
+        string save_count;
+        string map_name;
+        SaveIndex(int i, const string& n, const string& pt, const string& sc, const string& mn):data_id(i), name(n), play_time(pt), save_count(sc), map_name(mn){};
+        SaveIndex(){};
+    };
+//クラス変数
+private:
+    static const int MAX_SAVE_COUNT;
+
+//インスタンス変数
+private:
+    //グローバルセーブデータ
+    rapidjson::Document global;
+    //対象ローカルセーブデータ
+    int local_id;
+    //ローカルセーブデータ達
+    vector<rapidjson::Document> local;
+    //FileUtils
+    cocos2d::FileUtils* fu;
+
+//通常関数
+public:
+    //全ローカルデータのセット
+    void setLocalDataAll();
+    //メインとなるローカルデータのセット
+    void setMainLocalData(const int& id);
+    //ローカルセーブデータリストの取得
+    vector<SaveIndex> getSaveList();
+    //セーブ
+    void save(const int& id);
+private:
+    //グローバルデータのセット
+    bool setGlobalData();
+    //セーブデータを全初期化
+    void initializeFiles();
+    //文字列置換(後でUtilsに行くかも)
+    string strReplace(const string& pattern, const string& replacement, string target);
+    string getSprintf(const string& format, const string& str);
+    //JSONファイル読み込み
+    rapidjson::Document readJsonFile(const string& path);
+    //JSONファイル書き出し
+    void writeJsonFile(const string& path, const rapidjson::Document& doc);
+
 //singleton用関数
 public:
     static PlayerDataManager* getInstance();
@@ -23,7 +73,5 @@ private:
     PlayerDataManager(const PlayerDataManager& other){};              //コピーコンストラクタ
     PlayerDataManager& operator = (const PlayerDataManager& other); //代入演算子
     
-//
 };
-
 #endif /* defined(__LastSupper__PlayerDataManager__) */
