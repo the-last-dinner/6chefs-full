33306db6d17960e204fbbcdd17a98f5e3c9ecfd2
RyoyaIno <inos.sj@gmail.com>
2015-11-08 04:34:40 +0900

    MiniSelectorクラスの作成

---
+++ Classes/Layers/Menu/MiniSelector.cpp
@@ -0,0 +1,23 @@
+//
+//  MiniSelector.cpp
+//  LastSupper
+//
+//  Created by 猪野凌也 on 2015/11/08.
+//
+//
+
+#include "MiniSelector.h"
+
+MiniSelector::MiniSelector(){FUNCLOG}
+
+bool MiniSelector::init()
+{
+    FUNCLOG
+    
+    return true;
+}
+
+void MiniSelector::onIndexChanged(int newIdx, bool sound){}
+void MiniSelector::onSpacePressed(int idx){}
+void MiniSelector::onMenuKeyPressed(){}
+
