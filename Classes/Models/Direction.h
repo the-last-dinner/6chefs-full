0cc15a3af4a6a7d2b72a8ce40b4569dea8aab7df
Kohei Asami <mintomidnight@gmail.com>
2015-11-10 23:46:15 +0900

    Directionクラス生成

---
+++ Classes/Models/Direction.h
@@ -0,0 +1,14 @@
+//
+//  Direction.h
+//  LastSupper
+//
+//  Created by Kohei Asami on 2015/11/10.
+//
+//
+
+#ifndef __LastSupper__Direction__
+#define __LastSupper__Direction__
+
+#include <stdio.h>
+
+#endif /* defined(__LastSupper__Direction__) */
