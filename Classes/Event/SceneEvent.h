daf86eed95ff760a9b75a825bf916ca3684278ed
koheiasami <mintomidnight@gmail.com>
2015-12-06 21:57:48 +0900

    イベントクラスを整理

--- Classes/Event/SceneEvent.h
+++ Classes/Event/SceneEvent.h
@@ -100,39 +100,4 @@ private:
     virtual void run() override;
 };
 
-// CountDownイベント
-class CountDownEvent : public GameEvent
-{
-public:
-    CREATE_FUNC_WITH_PARAM(CountDownEvent, rapidjson::Value&)
-private:
-    float second { 5.f };
-    int sEventId { static_cast<int>(EventID::UNDIFINED) };
-    int fEventId { static_cast<int>(EventID::UNDIFINED) };
-    //rapidjson::Document doc { nullptr };
-    int equip {0};
-    bool checkEquip {false};
-    GameEvent* sEvent { nullptr };
-    GameEvent* fEvent { nullptr };
-    GameEvent* event { nullptr };
-private:
-    CountDownEvent() {FUNCLOG};
-    ~CountDownEvent() {FUNCLOG};
-    virtual bool init(rapidjson::Value& json);
-    virtual void run() override;
-};
-
-// StopCountイベント
-class StopCountEvent : public GameEvent
-{
-public:
-    CREATE_FUNC_WITH_PARAM(StopCountEvent, rapidjson::Value&);
-private:
-    StopCountEvent() {FUNCLOG};
-    ~StopCountEvent() {FUNCLOG};
-    virtual bool init(rapidjson::Value& json);
-    virtual void run() override;
-};
-
-
 #endif /* defined(__LastSupper__SceneEvent__) */
