60138a2106fe1beaf2dfa41ea761d3065a368bb7
RyoyaIno <inos.sj@gmail.com>
2015-12-25 00:53:06 +0900

    s_0をキャラメニューで表示することにして、キャラメニューに大宝が表示されないようにした

--- Classes/Layers/Menu/CharacterMenuLayer.cpp
+++ Classes/Layers/Menu/CharacterMenuLayer.cpp
@@ -163,7 +163,7 @@ void CharacterMenuLayer::changeCharaImage(const int idx)
     else
     {
         // 見ることができるキャラクター
-        string fileName = CsvDataManager::getInstance()->getCharaFileName(this->characters[idx]) + "_s_1.png";
+        string fileName = CsvDataManager::getInstance()->getCharaFileName(this->characters[idx]) + "_s_0.png";
         if(SpriteFrameCache::getInstance()->getSpriteFrameByName(fileName))
         {
             Sprite* img { Sprite::createWithSpriteFrameName(fileName)};
