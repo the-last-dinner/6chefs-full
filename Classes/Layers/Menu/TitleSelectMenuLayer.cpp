f288cfbd7b91d279d2ab76286aa1e60b6e7188fc
KoheiAsami2 <mintomidnight2@gmail.com>
2017-09-17 15:18:57 +0900

    背景の座標調整

--- Classes/Layers/Menu/TitleSelectMenuLayer.cpp
+++ Classes/Layers/Menu/TitleSelectMenuLayer.cpp
@@ -20,6 +20,8 @@ bool TitleSelectMenuLayer::init(function<void(int)> onSelectTitle)
     if (!MenuLayer::init(2, 1)) return false;
     _onSelectTitle = onSelectTitle;
     Sprite* bg { Sprite::createWithSpriteFrameName("title_select_1.png") };
+    Size bgSize { bg->getContentSize() };
+    bg->setPosition(bgSize.width / 2, bgSize.height / 2);
     this->addChild(bg);
     return true;
 }
