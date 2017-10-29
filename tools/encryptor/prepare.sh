894a11ab036d21e9c0c0f2d52839c3ea0ba56749
inosy22 <inos.sj@gmail.com>
2017-10-29 16:06:07 +0900

    リリースのためリソースの暗号化

--- tools/encryptor/prepare.sh
+++ tools/encryptor/prepare.sh
@@ -3,9 +3,9 @@
 ROOT_PATH=`pwd`
 
 ## 原始的だけど、シリーズごとにコメントアウト変える
-RESOUCES_PATH=${ROOT_PATH}/../../Resources/common
+# RESOUCES_PATH=${ROOT_PATH}/../../Resources/common
 # RESOUCES_PATH=${ROOT_PATH}/../../Resources/6chefs
-# RESOUCES_PATH=${ROOT_PATH}/../../Resources/6chefs2
+RESOUCES_PATH=${ROOT_PATH}/../../Resources/6chefs2
 TMP_PATH=${ROOT_PATH}/tmp
 
 # generate target list of encryption
